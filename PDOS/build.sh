b57125209dd78606c83aa628b04868978ce91d0b
PixelDevelopers <maximeruys20@gmail.com>
2017-08-07 22:49:43 +0200

    Add files via upload

--- PDOS/build.sh
+++ PDOS/build.sh
@@ -2,3 +2,7 @@ nasm -f elf32 kernel.asm -o kasm.o
 gcc -m32 -c kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o PDOS.bin kasm.o kc.o
 ld -m elf_i386 -T link.ld -o PDOS.img kasm.o kc.o
+ld -m elf_i386 -T link.ld -o PDOS.iso kasm.o kc.o
+qemu-system-i386 -kernel PDOS.bin
+
+read a
